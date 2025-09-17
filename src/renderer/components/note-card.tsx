82c111a9319c0047bb8abe29ce5aaed831f01471
furkankizilbuga <furkankizilbuga12@gmail.com>
2025-09-18 00:22:59 +0300

    added navigation to note details

--- src/renderer/components/note-card.tsx
+++ src/renderer/components/note-card.tsx
@@ -1,11 +1,14 @@
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
 } from "@/renderer/components/ui/card"
 import type { Note } from "@/renderer/types/note"
+import { useNavigate } from "@tanstack/react-router"
+import { Edit2 } from "lucide-react"
 
 type TProps = {
     note: Note
@@ -14,11 +17,14 @@ type TProps = {
 // TODO: Content'in başları title'da gözükecek eğer title yok ise.
 // card content içerisinde de truncate edilmiş content gösterelim
 export const NoteCard = ({ note }: TProps) => {
+    const navigate = useNavigate();
+    const handleNavigation = () => navigate({ to: `/notes/${note.externalId}`, replace: true });
     return (
         <Card className="h-40 w-36">
             <CardHeader>
                 <CardTitle>{note.title}</CardTitle>
                 <CardDescription>Card Description</CardDescription>
+                <CardAction onClick={handleNavigation}><Edit2 className='w-4 h-4 cursor-pointer' /></CardAction>
             </CardHeader>
             <CardContent>
                 <p>Card Content</p>
